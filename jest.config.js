b59f8cb1ad93172a0c111938ad403054cd135752
Zach Schneider <me@zachschneider.com>
2026-04-01 14:25:47 -0500

    upgraded all packages

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+  },
 };
